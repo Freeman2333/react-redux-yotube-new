63cf3ffe40a4bf00d23043b9fdd7935871b0b12c
Sergii Popov <freeman23@bigmir.net>
2021-06-08 07:49:34 +0300

    start sidebar

--- src/Router.js
+++ src/Router.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Sidebar from "./components/Sidebar";
 
 const AppRouter = () => {
   return (
     <Router>
       <Navbar />
+      <Sidebar />
       <Switch>
         <Route path="/" component={Home} />
       </Switch>
